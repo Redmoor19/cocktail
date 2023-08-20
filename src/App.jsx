29f7496203e50d76697af76d6e4cda92e6ca4fec
Redmoor19 <kald03@yandex.ru>
2023-08-20 22:51:24 +0300

    Router configured. React Query configured. Project stuctured.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import Browse from "./pages/Browse";
+import Cart from "./pages/Cart";
+import PageNotFound from "./pages/PageNotFound";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <>Coctail 552</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
