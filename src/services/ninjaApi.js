c61ae63421b7e49b23ff40122f52083bfab5f9de
Redmoor19 <kald03@yandex.ru>
2023-08-24 13:34:20 +0300

    test

--- src/services/ninjaApi.js
+++ src/services/ninjaApi.js
@@ -4,14 +4,14 @@ const API = import.meta.env.VITE_NINJA_KEY;
 
 export async function getJoke() {
   const data = await fetchData(`${BASE_URL}dadjokes?limit=1`, "GET", {
-    "X-Api-Key": API,
+    "X-Api-Key": "5Vv7Bdl4zoKvk/a0fGZ+OQ==LX0wv8Hm6C2J5di2",
   });
   return data[0];
 }
 
 export async function getFact() {
   const data = await fetchData(`${BASE_URL}facts?limit=1`, "GET", {
-    "X-Api-Key": API,
+    "X-Api-Key": "5Vv7Bdl4zoKvk/a0fGZ+OQ==LX0wv8Hm6C2J5di2",
   });
   return data[0];
 }
